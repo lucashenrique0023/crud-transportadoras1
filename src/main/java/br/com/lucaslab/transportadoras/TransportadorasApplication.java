19871dc6c8edaf7f8fc4d044e37615d6efb043bf
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-10 19:46:45 -0300

    first commit

---
+++ src/main/java/br/com/lucaslab/transportadoras/TransportadorasApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucaslab.transportadoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportadorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportadorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucaslab/transportadoras/TransportadorasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucaslab.transportadoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportadorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
